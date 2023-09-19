3b137d7245b65772b3ce093ea3c2f8b16814a0b0
hilmizul <netspytux@gmail.com>
2023-09-19 20:08:02 +0700

    change back button

--- pages/inventory/tambah.vue
+++ pages/inventory/tambah.vue
@@ -5,7 +5,7 @@
         <div class="card mb-5 text-white">
           <div class="card-header">
             <h4 class="title">
-              <a @click="() => router.back()" class="link text-white me-2">back</a>
+              <a @click="() => router.back()" class="link text-white me-2">◀</a>
               Tambah alat
               <span v-if="loading"><em>loading</em></span>
             </h4>
