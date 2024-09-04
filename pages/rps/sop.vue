e6806e233c84dcf9fc1d84b6f96ba355e8b05abd
hilmizul <netspytux@gmail.com>
2024-09-04 13:13:35 +0700

    feat: fetch from API

--- pages/rps/sop.vue
+++ pages/rps/sop.vue
@@ -7,46 +7,8 @@
             <h4 class="title">{{ title }}</h4>
           </div>
           <div class="card-body">
-            <p>Prosedur Operasi Standar / Tata Tertib Penggunaan RPS RPL SMKN 4 Tasikmalaya</p>
-            <ol>
-              <li>Waktu Operasional pukul 07.00 - 16.00 dan istirahat pukul 12.00 - 13.00 WIB</li>
-              <li>
-                Seluruh Siswa Laboratorium Komputer diwajibkan:
-                <ol type="a">
-									<li>Menggunakan seragam Praktik yang rapih selama Jadwal Praktik</li>
-                  <li>Mengisi <nuxt-link to="/rps/presensi">Presensi</nuxt-link> Penggunaan RPS selama KBM (Kegiatan Belajar Mengajar) oleh Sekretaris Kelas/Guru Mapel</li>
-                  <li>Mencuci tangan dengan sabun sebelum masuk kedalam Laboratorium</li>
-                  <li>Melepas alas kaki dan memakai kaos kaki yang bersih atau dilepas</li>
-                  <li>Membersihkan Laboratorium sebelum dan sesudah digunakan</li>
-                  <li>Menjaga kebersihan dan merapikan laboratorium sebelum keluar ruangan</li>
-                  <li>Mengkonfirmasi terlebih dahulu ke Laboran/Kabeng sebelum memakai ruangan</li>
-                  <li>Merawat dan menjaga semua fasilitas didalam Laboratorium</li>
-                  <li>Mengisi form peminjaman alat/ruangan di https://kabengrpl.netlify.app</li>
-                  <li>Mengganti barang yang rusak sesuai jumlah dan spesifikasi alat yang rusak</li>
-                </ol>
-              </li>
-              <li>
-                Seluruh Siswa Laboratorium Komputer dilarang:
-                <ol type="a">
-									<li>Merangkap seragam Praktik dengan seragam lain selama Jadwal Praktik (kecuali hari Senin)</li>
-                  <li>Membawa atau makan, minum dan merokok di Laboratorium</li>
-                  <li>Membuang sampah didalam Laboratorium dan toilet</li>
-                  <li>Membawa peralatan Laboratorium keluar ruangan Laboratorium tanpa izin Laobran</li>
-                  <li>Meninggalkan barang apapun milik pribadi tanpa seizin Laboran</li>
-                  <li>Meminjamkan alat (Komputer & Laptop) selain untuk pembelajaran</li>
-                  <li>Menulis/menggambar di dinding, meja dan alat di Laboratorium</li>
-                  <li>Berisik, memutar musik/karaoke saat jam operasional</li>
-                  <li>Dilarang membawa orang luar Sekolah</li>
-                  <li>Dilarang membawa orang luar Jurusan (terkecuali izin kepada Laboran/Kabeng)</li>
-                </ol>
-              </li>
-            </ol>
-            <div class="text-center">
-              <div>TTD</div>
-              <div>Tasikmalaya, 1 Januari 2022</div>
-              <div>Kepala Bengkel RPL,</div>
-              <div>Zul Hilmi</div>
-            </div>
+            <span v-html="content"></span>
+            <div class="mt-3"><em class="text-muted">Last update: {{ update }}</em></div>
           </div>
         </div>
       </div>
@@ -55,8 +17,28 @@
 </template>
 
 <script setup>
-const title = "SOP (Standard Operating Procedure)"
-useHead({ title: `${title} / Kabeng RPL` })
+const title = ref("Memuat...")
+const content = ref("Memuat...")
+const update = ref("Memuat...")
+const client = useSupabaseClient()
+
+async function getData() {
+  let { data, error } = await client
+    .from("inv_sop")
+    .select()
+    .single()
+  if(data) {
+    title.value = data.title
+    content.value = data.content
+    let date = new Date(Date.parse(data.update)).getDate()
+    let month = new Date(Date.parse(data.update)).toLocaleString('default', { month: 'long'})
+    let year = new Date(Date.parse(data.update)).getFullYear()
+    update.value = date + ' ' + month + ' ' + year
+  }
+}
+
+onMounted(() => getData())
+// useHead({ title: `${title} / Kabeng RPL` })
 </script>
 
 <style scoped>
