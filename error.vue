f5ea5028f9c696f2c5574e0f1b728e19150c6cab
hilmizul <netspytux@gmail.com>
2024-12-20 19:17:44 +0700

    add file: custom error page

---
+++ error.vue
@@ -0,0 +1,28 @@
+<template>
+  <div class="container">
+    <div class="row justify-content-center">
+      <div class="col-lg-4">
+        <div class="alert alert-light text-center my-5 shadow">
+          <h1 class="h1">{{ error?.statusCode }}</h1>
+          <div class="my-3">{{ error?.message }}</div>
+          <NuxtLink to="/" class="btn btn-dark rounded-5">kembali ke Home</NuxtLink>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script setup lang="ts">
+import type { NuxtError } from '#app';
+useHead({ title: "404: Page not found 👀" })
+const props = defineProps({
+  error: Object as () => NuxtError
+})
+</script>
+
+<style scoped>
+.alert {
+  border-radius: 0;
+  border: none;
+}
+</style>
